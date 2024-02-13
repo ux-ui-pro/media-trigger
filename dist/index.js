37189d862fb5c0695f51da9d6e21e4ab7b440130
ux-ui-pro <maxim@ux-ui.pro>
2024-02-13 21:21:10 +0300

    issue: made the parameters optional

--- dist/index.js
+++ dist/index.js
@@ -11,26 +11,32 @@ $parcel$defineInteropFlag(module.exports);
 
 $parcel$export(module.exports, "default", () => $4fa36e821943b400$export$2e2bcd8739ae039);
 class $4fa36e821943b400$var$MediaTrigger {
-    constructor({ media: media, entry: entry, exit: exit, change: change }){
+    #MQ;
+    #entry;
+    #exit;
+    #change;
+    #prev;
+    constructor({ media: media, entry: entry = null, exit: exit = null, change: change = null }){
         if (!window.matchMedia) return;
-        if (typeof entry !== "function" || typeof exit !== "function" || typeof change !== "function") return;
-        this.MQ = window.matchMedia(media);
-        this.handleChange = this.handleChange.bind(this);
-        this.MQ.addEventListener("change", this.handleChange);
-        this.entry = entry ?? null;
-        this.exit = exit ?? null;
-        this.change = change ?? null;
-        this.trigger(this.MQ);
+        this.#MQ = window.matchMedia(media);
+        this.#handleChange = this.#handleChange.bind(this);
+        this.#MQ.addEventListener("change", this.#handleChange);
+        this.#entry = entry;
+        this.#exit = exit;
+        this.#change = change;
+        this.#trigger(this.#MQ);
     }
-    trigger = ({ matches: matches })=>{
-        if (matches !== this.prev) {
-            matches ? this.entry?.(this.MQ) : this.exit?.(this.MQ);
-            this.change?.(this.MQ);
-            this.prev = matches;
+    #trigger = ({ matches: matches })=>{
+        if (matches !== this.#prev) {
+            if (matches) {
+                if (this.#entry) this.#entry(this.#MQ);
+            } else if (this.#exit) this.#exit(this.#MQ);
+            if (this.#change) this.#change(this.#MQ);
+            this.#prev = matches;
         }
     };
-    handleChange = ({ matches: matches })=>{
-        this.trigger({
+    #handleChange = ({ matches: matches })=>{
+        this.#trigger({
             matches: matches
         });
     };
--- src/index.js
+++ src/index.js
@@ -1,40 +1,49 @@
 class MediaTrigger {
-	constructor({ media, entry, exit, change }) {
+	#MQ;
+	#entry;
+	#exit;
+	#change;
+	#prev;
+
+	constructor({ media, entry = null, exit = null, change = null }) {
 		if (!window.matchMedia) {
 			return;
 		}
 
-		if (
-			typeof entry !== 'function' ||
-			typeof exit !== 'function' ||
-			typeof change !== 'function'
-		) {
-			return;
-		}
+		this.#MQ = window.matchMedia(media);
+		this.#handleChange = this.#handleChange.bind(this);
 
-		this.MQ = window.matchMedia(media);
-		this.handleChange = this.handleChange.bind(this);
+		this.#MQ.addEventListener('change', this.#handleChange);
 
-		this.MQ.addEventListener('change', this.handleChange);
+		this.#entry = entry;
+		this.#exit = exit;
+		this.#change = change;
 
-		this.entry = entry ?? null;
-		this.exit = exit ?? null;
-		this.change = change ?? null;
-
-		this.trigger(this.MQ);
+		this.#trigger(this.#MQ);
 	}
 
-	trigger = ({ matches }) =>  {
-		if (matches !== this.prev) {
-			matches ? this.entry?.(this.MQ) : this.exit?.(this.MQ);
+	#trigger = ({ matches }) => {
+		if (matches !== this.#prev) {
+			if (matches) {
+				if (this.#entry) {
+					this.#entry(this.#MQ);
+				}
+			} else {
+				if (this.#exit) {
+					this.#exit(this.#MQ);
+				}
+			}
 
-			this.change?.(this.MQ);
-			this.prev = matches;
+			if (this.#change) {
+				this.#change(this.#MQ);
+			}
+
+			this.#prev = matches;
 		}
 	}
 
-	handleChange = ({ matches }) =>  {
-		this.trigger({ matches });
+	#handleChange = ({ matches }) => {
+		this.#trigger({ matches });
 	}
 }
 
